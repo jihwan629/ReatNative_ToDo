b0e8f4a7c674c6663d481489c80dbd8a0d2fa59a
jihwan629 <jihwan629@naver.com>
2021-04-09 19:48:58 +0900

    할 일 추가 화면 만들기

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './components/Header'
 import TodoItem from './components/TodoItem'
+import TaskModal from './components/TaskModal'
+import Modal from 'react-native-modal'
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
       <Header />
       <TodoItem title="클래스101 커리큘럼 만들기" done={true} />
       <TodoItem title="운전면허 도로주행 연수" done={false} />
+      <TaskModal isVisible={false} />
     </SafeAreaView>
   );
 }
