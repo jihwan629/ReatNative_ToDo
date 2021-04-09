527699bd9a9cc6e2f17e80f1d973206b1c015388
jihwan629 <jihwan629@naver.com>
2021-04-09 21:06:38 +0900

    할 일 삭제하기

--- App.js
+++ App.js
@@ -31,11 +31,16 @@ export default class App extends React.Component {
 
         <FlatList
           data={this.state.todos}
-          renderItem={({ item }) => {
+          renderItem={({ item, index }) => {
             return (
               <TodoItem
                 title={item.title}
                 done={item.done}
+                remove={() => {
+                  this.setState({
+                    todos: this.state.todos.filter((_, i) => i !== index)
+                  })
+                }}
               />
             )
           }}
