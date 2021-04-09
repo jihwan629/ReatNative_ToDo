d6e0d069384db15d958ab1320e28ca0df694245d
jihwan629 <jihwan629@naver.com>
2021-04-09 21:00:27 +0900

    할 일 추가하기

--- components/TaskModal.js
+++ components/TaskModal.js
@@ -8,8 +8,10 @@ import Modal from 'react-native-modal'
 
 const TaskModal = ({
     isVisible,
+    add,
     hide,
 }) => {
+    let content = ''
     return (
         <Modal
             isVisible={isVisible}
@@ -19,6 +21,10 @@ const TaskModal = ({
         >
             <View style={styles.container}>
                 <TextInput 
+                    onChangeText={(text) => {
+                        content = text
+                    }}
+                    onEndEditing={() => add(content)}
                     placeholder="새로운 할 일을 추가해 주세요"
                 />
             </View>
